95b539bdf3a5d21e7f2a3c64d0762b0124973a22
Travis James <travis@tribehealthsolutions.com>
2025-05-27 21:23:49 -0500

    fixed proxy

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,27 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    cors: {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+      allowedHeaders: "*"
+    },
   },
   preview: {
     allowedHosts: ["ai.brius.com"],
     host: "::",
     port: 4173,
+    cors: {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+      allowedHeaders: "*"
+    },
   },
   plugins: [
     react(),
